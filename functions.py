860f744b8f2a80c95f29c9f074115cb71085dc6b
Gizem Semerci <39341967+gizemdoruk@users.noreply.github.com>
2023-09-04 15:51:22 +0300

    funcitons cretaed

--- functions.py
+++ functions.py
@@ -1,18 +1,39 @@
-def pi_organizer(pi):
-    if pi>3:
-        print("buyuktur uc")
+def pi_organizer(pi=0):
+
+    if pi == 3.1415:
+        print("Pi NUMBER IS Correct")
         return True
-    elif pi 3<=3:
-        print("buyuk degıldır uc")
+    elif pi <= 3 :
+        print("Pi NUMBER IS NOT Correct")
         return False
+    else :
+        print("Not any information") 
+        return False
+    return pi
+
+def is_capital(city_name,capitals):
+    if city_name in capitals:
+        print(city_name + " yes it is!")
+        return True
+    elif city_name.capitalize() in capitals:
+        print(city_name.capitalize() + " yes but not actualy")
+        return True
     else:
-        print("tanımsız")
         return False
-    
 
-input=[3, 6, 12,4,324,1.435,3.1415]
 
-for inp in input:
+def my_filter(input,cptls):
+    if isinstance(input,str):
+        out = is_capital(input,capitals=cptls)
+    else:
+        out = pi_organizer(input)
+    return out
 
-output= pi_organizer(pi=inp)
-print(output)
+capitals = ["Seul","Berlin","Tokyo"]
+
+inputs = ["İstanbul",2,64,"berlin",3.1415,"tokyo",3,2,3,"Seul"]
+
+for input in inputs:
+
+    output= my_filter(input,capitals)
+    print(output)
